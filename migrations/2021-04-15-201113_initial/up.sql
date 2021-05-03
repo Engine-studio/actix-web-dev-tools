76e74e3f824228b0429d572832a7e6e28a583b29
badconfig <array.clean@gmail.com>
2021-05-03 22:35:50 +0300

    sd

--- migrations/2021-04-15-201113_initial/up.sql
+++ migrations/2021-04-15-201113_initial/up.sql
@@ -1,8 +1,7 @@
 -- Your SQL goes here
 CREATE TABLE Auth (
-    id uuid DEFAULT uuid_generate_v4 (),
+    id BIGSERIAL PRIMARY KEY,
     login VARCHAR NOT NULL,
     auth_type VARCHAR NOT NULL,
     roles TEXT[] NOT NULL 
-    PRIMARY KEY (id)
 );
