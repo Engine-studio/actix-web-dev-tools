ea42c949ca495126f0ab275ef3e500833492d4b1
badconfig <array.clean@gmail.com>
2021-05-03 23:44:14 +0300

    asdf

--- migrations/2021-04-15-201113_initial/up.sql
+++ migrations/2021-04-15-201113_initial/up.sql
@@ -1,7 +1,7 @@
 -- Your SQL goes here
 CREATE TABLE Auth (
     id BIGSERIAL PRIMARY KEY,
-    login VARCHAR NOT NULL,
+    login VARCHAR NOT NULL UNIQUE,
     auth_type VARCHAR NOT NULL,
     roles TEXT[] NOT NULL 
 );
